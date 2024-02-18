594b66aeff1a59c1462d42559031c5ee1d38ac7e
TavoV21 <gustavoadolfovilladacamargo@gmail.com>
2024-02-17 20:31:50 -0500

    put seeker

--- mernfront/src/pages/listaUsuarios.js
+++ mernfront/src/pages/listaUsuarios.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Swal from 'sweetalert2';
 import {Link} from 'react-router-dom';
-import details from './details.module.css';
 import {getToken} from '../components/auth-helper';
+import details from './details.module.css';
 
-/* import UsuarioIndividual from './usuarioIndividual';
- */
 function ListaUsuarios(){
 
   const token = getToken();
   const [dataUsuarios, setDataUsuarios]=useState([]);
+  const [buscar, setBuscar] = useState('');
   
   useEffect(() => {
    Axios.get('api/usuario/obtenerUsuarios').then(res=>{
@@ -45,12 +44,30 @@ function ListaUsuarios(){
       });
 
     }  
+    
+      let results = [];
 
+      if (!buscar) {
+        results= dataUsuarios;
+      }else{
+        results = dataUsuarios.filter((data)=>
+        data.nombre.toLowerCase().includes(buscar.toLocaleLowerCase())
+        )
+      }
+    
   return <div className={details.wraplist}>
     {token?
-  <div>
+  <div className="row justify-content-center">
+
   <h2>Lista de Usuarios</h2>
-    <div className='d-flex justify-content-center'>
+
+<div className={details.inputwrapper}>
+  <input type="text" class="form-control" className={details.inputsearch} placeholder="Buscar..." aria-label="Recipient's username" aria-describedby="button-addon2" onChange={(e)=>{setBuscar(e.target.value)}} value={buscar}/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={details.inputicon} viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+</svg>
+</div>
+
     <table className="table w-75">
     <thead>
       <tr>
@@ -63,7 +80,7 @@ function ListaUsuarios(){
     </thead>
     <tbody>
     {
-        dataUsuarios.map(usuario=>(
+        results.map(usuario=>(
           <tr key={usuario.id}>
           <td>{usuario.id}</td>
           <td>{usuario.nombre}</td>
@@ -80,7 +97,6 @@ function ListaUsuarios(){
     </tbody>
     </table>
 
-</div>
 </div>:
 <div><h1>No autorizado</h1></div> 
   }
